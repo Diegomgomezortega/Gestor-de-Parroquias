2a233b75bf67af5f4df8ae623621a3bce2e436a4
Diego Gómez <65495810+Diegomgomezortega@users.noreply.github.com>
2021-11-18 20:17:08 -0300

    Creacion Clase Catequista

---
+++ Entidades/Catequista.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Catequista: Persona
+    {
+        #region Atributos
+        private int id_Catequista;
+        #endregion
+        #region Propiedades
+        public int Id_Catequista
+        {
+            get { return id_Catequista; }
+            set { id_Catequista = value; }
+        }
+        #endregion
+        #region Constructores
+        public Catequista()//Constructor por defecto
+        {
+
+        }
+        public Catequista(int Id_Catequista)//Inicializa solo su id
+        {
+            id_Catequista = Id_Catequista;
+            
+        }
+        public Catequista(int Du, string Nom, string Apel, string Dir, DateTime FecNac, int Tel, char Sex, int Id_Cat) : base(Du, Nom, Apel, Dir, Tel, FecNac, Sex)
+        {
+            Dni = Du;
+            Nombre = Nom;
+            Apellido = Apel;
+            Direccion = Dir;
+            FechNac = FecNac;
+            Telefono = Tel;
+            Sexo = Sex;
+            Id_Catequista = Id_Cat;
+            
+
+        }//Inicializa todos sus atributos, y los de la base Personas tambien
+        #endregion
+    }
+}
