acfdcde19c4ed6f321bf35c006f3982d3b08792e
Diego Gómez <65495810+Diegomgomezortega@users.noreply.github.com>
2021-11-18 17:14:10 -0300

    Creacion Clase Persona, Catecumeno, Agregado de Referencias

---
+++ Entidades/Catecumeno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Catecumeno: Persona
+    {
+        #region Atributos
+        private int id_Catecumeno;
+        private int cod_Catequesis;
+        #endregion
+        #region Propiedades
+        public int Id_Catecumeno
+        {
+            get { return id_Catecumeno; }
+            set { id_Catecumeno = value; }
+        }
+        public int Cod_Catequesis
+        {
+            get { return cod_Catequesis; }
+            set { cod_Catequesis = value; }
+        }
+        #endregion
+        #region Constructores
+        public Catecumeno()//Constructor por defecto
+        {
+
+        }
+        public Catecumeno(int Id_Catecumeno, int Cod_Catequesis)//Inicializa solo su id y y el id de la catequesis
+        {
+            id_Catecumeno = Id_Catecumeno;
+            cod_Catequesis = Cod_Catequesis;
+        }
+        public Catecumeno(int Du, string Nom, string Apel, string Dir, DateTime FecNac, int Tel, char Sex, int Id_Cat, int Cod_Cat) : base(Du, Nom, Apel, Dir, Tel, FecNac, Sex)
+        {
+            Dni = Du;
+            Nombre = Nom;
+            Apellido = Apel;
+            Direccion = Dir;
+            FechNac = FecNac;
+            Telefono = Tel;
+            Sexo = Sex;
+            Id_Catecumeno = Id_Cat;
+            Cod_Catequesis = Cod_Cat;
+
+
+
+        }//Inicializa todos los atributos, y los de la base Personas tambien
+        #endregion
+    }
+}
---
+++ Entidades/Persona.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Persona
+    {
+        #region Atributos
+        private int dni;
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private int telefono;
+        private DateTime Fecha_nac;
+        private char sexo;
+        #endregion
+        #region Propiedades Encapsulamiento
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+        public DateTime FechNac
+        {
+            get { return Fecha_nac; }
+            set { Fecha_nac = value; }
+        }
+        public char Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public int Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+        #endregion
+        #region Constructores
+        public Persona()//por defecto
+        { }
+        public Persona(int Dni, string Nom, string Apel, string Dir, int Tel, DateTime FecNac, char Sex)//Contructor hecho por programador
+        {
+
+            nombre = Nom;
+            dni = Dni;
+            apellido = Apel;
+            direccion = Dir;
+            telefono = Tel;
+            Fecha_nac = FecNac;
+            sexo = Sex;
+        }
+        #endregion
+        #region Metodos
+        public int Edad()
+        {
+            int edad;
+            edad = FechNac.Year - DateTime.Now.Year;
+            return edad;
+        }
+        #endregion
+    }
+}
