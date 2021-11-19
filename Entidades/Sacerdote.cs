17a336459e8ea1593a5368120941c5d16efe3b81
Diego Gómez <65495810+Diegomgomezortega@users.noreply.github.com>
2021-11-18 21:31:18 -0300

    Creacion Sacerdote

---
+++ Entidades/Sacerdote.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sacerdote: Persona
+    {
+        private int id_Sacerdote;
+
+        public int Id_Sacerdote { get => id_Sacerdote; set => id_Sacerdote = value; }
+
+        public Sacerdote()
+        {
+
+        }
+        public Sacerdote(int ID_Sac)
+        {
+            id_Sacerdote = ID_Sac;
+        }
+        public Sacerdote(int Du, string Nom, string Apel, string Dir, DateTime FecNac, int Tel, char Sex, int Id_Sac) : base(Du, Nom, Apel, Dir, Tel, FecNac, Sex)
+        {
+
+            Dni = Du;
+            Nombre = Nom;
+            Apellido = Apel;
+            Direccion = Dir;
+            FechNac = FecNac;
+            Telefono = Tel;
+            Sexo = Sex;
+            Id_Sacerdote = Id_Sac;
+
+        }
+    }
+}
